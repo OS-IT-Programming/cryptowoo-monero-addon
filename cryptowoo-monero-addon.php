2f01689a5ceb21098638ab75dbfd8c6db5025f3e
OlavOlsm <OlavOlsm@gmail.com>
2020-12-30 23:25:39 +0100

    Make sure CryptoID token field is always at the end of processing-api

--- cryptowoo-monero-addon.php
+++ cryptowoo-monero-addon.php
@@ -1126,6 +1126,8 @@ function cwxmr_add_fields() {
 
 	// Remove blockcypher token field
 	Redux::removeField( 'cryptowoo_payments', 'blockcypher_token', false );
+	// Remove CryptoID token field
+	Redux::removeField( 'cryptowoo_payments', 'cryptoid_api_key', false );
 
 	/*
 	 * Processing API
@@ -1208,6 +1210,17 @@ function cwxmr_add_fields() {
 		'title'             => __( 'BlockCypher Token (optional)', 'cryptowoo' ),
 		'subtitle'          => sprintf( __( 'Use the API token from your %sBlockCypher%s account.', 'cryptowoo' ), '<strong><a href="https://accounts.blockcypher.com/" title="BlockCypher account vtcboard" target="_blank">', '</a></strong>' ),
 		'validate_callback' => 'redux_validate_token'
+	) );
+	// Re-add CryptoID token field
+	Redux::setField( 'cryptowoo_payments', array(
+		'section_id' => 'processing-api',
+		'id'         => 'cryptoid_api_key',
+		'type'       => 'text',
+		'ajax_save'  => false, // Force page load when this changes
+		'desc'       => sprintf(__('%sMore info%s', 'cryptowoo'), '<a href="https://chainz.cryptoid.info/api.dws" title="cryptoID API Docs" target="_blank">', '</a>'),
+		'title'      =>  __('cryptoID API Key (required)', 'cryptowoo'),
+		'subtitle'   => sprintf(__('Use the API token from your %sCryptoID%s account.', 'cryptowoo'), '<strong><a href="https://chainz.cryptoid.info/api.key.dws" title="Request cryptoID API Key" target="_blank">', '</a></strong>'),
+		//'validate_callback' => 'redux_validate_token',
 	) );
 
 	/*
